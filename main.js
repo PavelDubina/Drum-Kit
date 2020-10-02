adf8c8efb7ecdc7ad4b487ceb9aa1ea6801f0dd1
Pavel Dubina <Pavel_Dubina@epam.com>
2020-10-02 10:56:04 +0300

    feat: finish

---
+++ main.js
@@ -0,0 +1,33 @@
+function playSound(e) {
+  const key = document.querySelector(`.key[data-key="${e.code}"]`),
+    audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+function playMouse(e) {
+    const audio = document.querySelector(`audio[data-key="${e.currentTarget.attributes[0].value}"]`);
+ audio.currentTime = 0;
+ audio.play();
+ this.classList.add('playing');
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+keys.forEach((key) => key.addEventListener("click", playMouse));
+window.addEventListener("keydown", playSound);
+
+
+
+
+
+ 
+
+
